d1eb37e23be6f23941ca95a70fca2e22b58846ad
denys <dp070999eda1@gmail.com>
2022-07-28 17:30:13 +0300

    My homework

---
+++ lessons1.py
@@ -0,0 +1,17 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+live = input("Where do you live? ")
+
+print("Hello, " + name + " your age is " + age + " you live in " +live)
+
+
+
+
+a = input("Write number a: " )
+b = input("Write number b: " )
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a)  / int(b))
+print(int(a)  * int(b))
+print(int(a) **int(b))
